8ee309e1bdbdcbee24e72225d6abd806cdf4c7ef
Victoria Fakunle <v.fakunle@alustudent.com>
2024-03-29 16:49:19 +0000

    eza_tech_app.py

--- eza_tech_app.py
+++ eza_tech_app.py
@@ -8,3 +8,15 @@ class EzaTechApp(cmd.Cmd):
         self.logged_in = False
         self.username = None
         self.user_data = {}
+def print_menu(self):
+        print("Eza-Tech Menu")
+        print("============")
+        print("1. Watch introduction video")
+        print("2. Register")
+        print("3. Login")
+        print("4. Logout")
+        print("5. Add farming data")
+        print("6. Add livestock data")
+        print("7. Access data")
+        print("8. Exit")
+
